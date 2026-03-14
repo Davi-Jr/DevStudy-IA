b0a0696be7acb25158e493e2c31bbe007d613161
Davi Jr <davibritojunior1@gmail.com>
2026-03-14 15:34:51 -0300

    :bricks: `feat` add roadmap page route

--- DevStudy-IA/src/App.tsx
+++ DevStudy-IA/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import HomePage from './components/HomePage';
 import DashBoardPage from './components/DashBoardPage';
+import RoadmapPage from './components/RoadmapPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<DashBoardPage />} />
+        <Route path="/roadmaps" element={<RoadmapPage />} />
       </Routes>
     </Router>
   );
