558e36a3e4e5082b4284e46067f45a9bccbdc531
Davi Jr <davibritojunior1@gmail.com>
2026-05-06 16:41:19 -0300

    `refactor`(ui): remover importação não utilizada do Link

--- DevStudy-IA/src/App.tsx
+++ DevStudy-IA/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from './lib/i18n';
 import LoginPage from './components/LoginPage';
 import HomePage from './components/HomePage';
